e84cf658fde989a1b85042561f80925f9cac34a4
giddyPwild <giddygbenga4@gmail.com>
2022-09-06 04:37:33 +0100

    Getting Started

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  // mode: 'jit',
+  purge: [
+    //   './public/**/*.{html.js}',
+    //   './public/index.html'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+}
